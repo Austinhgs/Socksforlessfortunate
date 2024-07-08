beb739df98efb3e68ec0125427d9936e2c68bb54
Admin <admin@The-Hidden-Genius-Project.local>
2024-07-08 12:02:25 -0700

    first commit

---
+++ atm.py
@@ -0,0 +1,29 @@
+import getpass 
+username="austin"
+password=12345
+
+
+def withdrawl():
+    withdraw=int(input( "how much do you want to deposit?"))
+    balance=600+deposit_cash
+def balance():
+    balance=600
+
+    def main():
+        print("welcome to austins atm:")
+        username=input ("What is your username?")
+        password=getpass.getpass()
+        if (username==username and password==password1):
+            print("password correct you may continue:")
+        else:
+            print("password or username incorrect")
+            print(username,password)
+            exit()
+        Answer=input("do you want to deposit, withdraw, or see balance.")
+        if(answer=="I"):
+           deposit()
+        elif(Answer=="J"):
+
+
+           print ("your balance is", balance)
+           main()
---
+++ doordash.py
@@ -0,0 +1,10 @@
+print(fruits)
+[outputs=Apple, berry, orange, watermelon]
+print(lens(fruits))
+print(type(fruits))
+elemts 0,1,2,3
+index 1,2,3,4
+output=berry 
+print (fruits)
+print(fruits(4:3))
+list
